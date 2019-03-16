9c41dfa92bb31c3c710bc119291ecd9266be0344
Saumya29 <saumyatiwari.29@gmail.com>
2019-03-16 23:25:25 +0530

    Added basic display using default props

---
+++ src/components/recipe.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './recipe.css';
+
+class Recipe extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+    instructions: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+  }
+  render() {
+    const {title, instructions, img} = this.props;
+    const ingredients = this.props.ingredients.map((ingred, index) => (
+      <li key={index}>{ingred}</li>
+    ));
+    return (
+      <div className="recipe-card">
+        <div className="recipe-card-image">
+          <img src={img} alt={title} />
+        </div>
+        <div className="recipe-card-content">
+          <h3 className="recipe-title">Recipe {title}</h3>
+          <h4>Ingredients:</h4>
+          <ul>{ingredients}</ul>
+          <h4>Instructions:</h4>
+          <p>{instructions}</p>
+        </div>
+      </div>
+     );
+  }
+}
+
+export default Recipe;
