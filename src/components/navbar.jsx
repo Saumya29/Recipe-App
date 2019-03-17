eac7767497cb5238d93b8c9cedc1ae1706d37110
Saumya29 <saumyatiwari.29@gmail.com>
2019-03-17 14:23:25 +0530

    Show/Hide form

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './navbar.css';
 
 class NavBar extends Component {
+  static defaultProps = {
+    onNewRecipe() {}
+  }
+
+  static propTypes = {
+    onNewRecipe: PropTypes.func,
+  }
+
   render() {
     return (
       <header>
         <h2><a>Recipe App</a></h2>
         <nav>
-          <li><a>New Recipe</a></li>
+          <li onClick={this.props.onNewRecipe}><a>New Recipe</a></li>
           <li><a>Home</a></li>
           <li><a>About</a></li>
           <li><a>Contact Us</a></li>
